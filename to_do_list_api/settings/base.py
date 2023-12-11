605faa0632f0400eccbc6ae4e4e2904a36826b66
gchsantos <gchsantos@gmail.com>
2023-12-10 22:35:44 -0300

    cd: adding rest framework and dacite dependencies

--- to_do_list_api/settings/base.py
+++ to_do_list_api/settings/base.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'board',
 ]
 
@@ -113,3 +114,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "to_do_list_api.authentications.BearerAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAdminUser"),
+}
