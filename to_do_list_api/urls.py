d0f9b66da0a722a1cbd627e267f128ae978204ce
gchsantos <gchsantos@gmail.com>
2023-12-13 03:01:26 -0300

    feat: creating auth class and installing in project

--- to_do_list_api/urls.py
+++ to_do_list_api/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-# from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.authtoken.views import obtain_auth_token
 
-# from .views import UserCreate
+from .views import UserCreate
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("account/register", UserCreate.as_view(), name="Register"),
-    # path("account/auth", obtain_auth_token, name="Authenticate"),
+    path("account/register", UserCreate.as_view(), name="Register"),
+    path("account/auth", obtain_auth_token, name="Authenticate"),
     path("api/board", include("board.urls")),
 ]
