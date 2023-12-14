236319021c648d84516acfe43e2e29159f822f96
gchsantos <gchsantos@gmail.com>
2023-12-14 02:36:04 -0300

    feat(board): add BoardManager route with task_id in url

--- board/urls.py
+++ board/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import BoardManager
+from to_do_list_api.constants import UUID_REGEX
 
 urlpatterns = [
     path("", BoardManager.as_view(), name="Board Manager"),
+    re_path(
+        rf"^/(?P<task_id>{UUID_REGEX})",
+        BoardManager.as_view(),
+        name="Board Manager",
+    ),
 ]
