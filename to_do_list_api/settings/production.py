779541ab8557680e6d427951a43b6f1c2c6a56aa
gchsantos <gchsantos@gmail.com>
2023-12-15 09:03:15 -0300

    feat: setting TTL to 30 seconds for validation

--- to_do_list_api/settings/production.py
+++ to_do_list_api/settings/production.py
@@ -6,4 +6,4 @@ from to_do_list_api.settings.base import *
 DEBUG = False
 ALLOWED_HOSTS = ["localhost"]
 BASE_DIR = os.path.dirname(os.path.realpath(os.path.dirname(__file__) + "/.."))
-CACHE_TTL = 60 * 15
+CACHE_TTL = 30 #60 * 15 I set the TTL to 30 seconds to validate the data
